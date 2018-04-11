4b03a96c18f802d1fb89d987fd2c7de495031429
Joshua Arnett <Josharnett850@gmail.com>
2018-04-10 21:12:58 -0400

    adding submit button click event and configuring and adding firebase database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-    console.log("ready");
+    
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDzO5oWQdQ3Yl75KRyZ8Z9G5Eg0dUOVwfw",
+        authDomain: "trainapp-2990b.firebaseapp.com",
+        databaseURL: "https://trainapp-2990b.firebaseio.com",
+         projectId: "trainapp-2990b",
+        storageBucket: "",
+        messagingSenderId: "1027838223037"
+    };
+    firebase.initializeApp(config);
 
+    const database = firebase.database();
 
     $("#submit").on("click", function(e) {
         e.preventDefault();
@@ -8,5 +19,13 @@ $(document).ready(function() {
         let destination = $("#destinationInput").val().trim();
         let trainTime = $("#timeInput").val().trim();
         let frequency = $("#frequencyInput").val().trim();
+
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            trainTime: trainTime,
+            frequency: frequency    
+        });
+
     });
 });
