ab7514214520e6ac372d95d3138ef568622c60de
Joshua Arnett <Josharnett850@gmail.com>
2018-04-11 21:37:08 -0400

    adding background image and jumbotron image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,6 +26,8 @@ $(document).ready(function() {
             trainTime: trainTime,
             frequency: frequency    
         });
+
+        $("form").trigger("reset");
     });
 
     database.ref().on("child_added", function(snapshot) {
@@ -34,5 +36,41 @@ $(document).ready(function() {
         let tDestination = $("<td>");
         let tTime = $("<td>");
         let tFrequency = $("<td>");
+        let minutesRemaining = $("<td>");
+
+        let firstTrainTime = snapshot.val().trainTime;
+        let timeFreq = parseInt(snapshot.val().frequency);
+
+        let firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+
+        let currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        let tRemainder = diffTime % timeFreq;
+        console.log(tRemainder);
+
+        let tMinutesTillTrain = timeFreq - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        
+        tRow.append(tName, tDestination, tFrequency, tTime, minutesRemaining);
+        $("#stats").append(tRow);
+
+        $(tName).html(snapshot.val().trainName);
+        $(tDestination).html(snapshot.val().destination);       
+        $(tFrequency).html(firstTrainTime);  
+        $(tTime).html(nextTrain);
+        $(minutesRemaining).html(tMinutesTillTrain);
     });
+
+
+    
+
+
 });
